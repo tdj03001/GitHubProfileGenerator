62b87210611789ecd88143bdf742bc0dd72f7eb0
tdj03001 <tyjenki@gmail.com>
2020-04-14 13:14:49 -0400

    Changed prompted questions in index.js to be stored in a variable so I could destructure the object later. Removed writeFileAsync since PDF generation doesn't need it. Refactored how promptUser() is invoked now that writeFileAsync is gone.

--- index.js
+++ index.js
@@ -2,38 +2,49 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const generateHTML = require("./generateHTML");
+const api = require("./api");
+const convertFactory = require("electron-html-to");
+const path = require("path");
 
-const writeFileAsync = util.promisify(fs.writeFile);
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
 
+  {
+    type: "list",
+    name: "color",
+    message: "What is your favorite color?",
+    choices: ["red", "blue", "green", "pink"]
+  }
+];
 
 function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "github",
-      message: "What is your GitHub username?"
-    },
+  inquirer.prompt(questions).then(({ github, color }) => {
+    console.log("Searching GitHub...");
 
-    {
-      type: "list",
-      name: "color",
-      message: "What is your favorite color?",
-      choices: ["red", "blue", "green", "pink"]
-    }
-  ]);
-}
+    api
+      .getUsername(github).then(response =>
+        api.getStarCount(github).then(starCount => {
+          return generateHTML({ starCount, color, ...response.data });
+        })
+      ).then(html => {
+        const conversion = convertFactory({
+          converterPath: convertFactory.converters.PDF
+        });
 
+        conversion({ html }, function (err, result) {
+          if (err) {
+            return console.log(err);
+          }
 
-
-promptUser()
-  .then(function (answers) {
-    const html = generateHTML(answers);
-
-    return writeFileAsync("index.html", html);
-  })
-  .then(function () {
-    console.log("Successfully wrote to index.html");
-  })
-  .catch(function (err) {
-    console.log(err);
+          result.stream.pipe(fs.createWriteStream(path.join(__dirname, "profile.pdf")));
+          conversion.kill();
+        });
+      });
   });
+};
+
+promptUser();
